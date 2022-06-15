23a2735ee7d8076c2077eed3140e6efeea95c0e8
HarmonicPillarar <106080482+HarmonicPillarar@users.noreply.github.com>
2022-06-15 19:24:10 +0300

    Add files via upload

--- MSAMS.ino
+++ MSAMS.ino
@@ -19,38 +19,21 @@
 */
 
 bool cleared = false;
-const int rowSpan = 16;
-int bs, p1s, p2s, p3s, p4s;
-String channelA = "", channelB = "", channelC = "", channelD = "", channelE = "";
-Button b(6);
-Potentiometer p1(A0);
-Potentiometer p2(A1);
-Potentiometer p3(A2);
-Potentiometer p4(A3);
+const int colSpan = 32;
+int bs, ps[4];
+char a = 'a', b = 'b', c = 'c', d = 'd', e = 'e';
+String channel[5] = {"", "", "", ""};
+Button btn(6);
+Potentiometer p[4] = {A0, A1, A2, A3};
 OLED display(
-  18,
-  19,
+  4,
+  2,
   NO_RESET_PIN,
   OLED::W_128,
   OLED::H_64,
   OLED::CTRL_SSD1306,
   0x3c
 );
-
-void setup() {
-  Serial.begin(115200);
-  display.begin();
-  MSAMSComponent *bc = &b;
-  MSAMSComponent *p1c = &p1;
-  MSAMSComponent *p2c = &p2;
-  MSAMSComponent *p3c = &p3;
-  MSAMSComponent *p4c = &p4;
-  bc->cfg();
-  p1c->cfg();
-  p2c->cfg();
-  p3c->cfg();
-  p4c->cfg();
-}
 
 void charStr(int num, OLED &display_, int x = 0, int y = 0) {
   String sn = String(num);
@@ -60,42 +43,59 @@ void charStr(int num, OLED &display_, int x = 0, int y = 0) {
     if(i == size_) getNum[i] = '\0';
     else getNum[i] = sn[i];
   }
-  display_.draw_string(x, y, getNum);
+  const char num_ = getNum;
+  display_.draw_string(x, y, num_);
 }
 
 void charLine(int num, OLED &display_, int x = 0, int y = 0) {
-  int mn = map(num, 0, 1023, x, OLED::W_128);
-  display_.draw_line(x, y, mn, y);
+  int mn = map(num, 0, 1023, OLED::H_64 - 1, 0);
+  display_.draw_line(x, mn, x, y);
+}
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial) {
+    ;
+  }
+  display.begin();
+  MSAMSComponent *bc = &btn;
+  bc->cfg();
+  for(int i = 0; i < 4; i++) {
+    MSAMSComponent *pc = p + i;
+    pc->cfg();
+  }
 }
 
 void loop() {
-  p1s = p1.getState();
-  channelA = (String)p1s;
-  channelA = 'a' + channelA;
-  p2s = p2.getState();
-  channelB = (String)p2s;
-  channelB = 'b' + channelB;
-  p3s = p3.getState();
-  channelC = (String)p3s;
-  channelC = 'c' + channelC;
-  p4s = p4.getState();
-  channelD = (String)p4s;
-  channelD = 'd' + channelD;
-  bs = b.getState();
-  channelE = (String)bs;
-  channelE = 'e' + channelE;
-  
+  for(int i = 0; i < 4; i++) {
+    ps[i] = p[i].getState();
+    channel[i] = (String)ps[i];
+    switch(i) {
+      case 0:
+        channel[i] = a + channel[i];
+        break;
+      case 1:
+        channel[i] = b + channel[i];
+        break;
+      case 2:
+        channel[i] = c + channel[i];
+        break;
+      case 3:
+        channel[i] = d + channel[i];
+        break;
+    }
+  }
+  bs = btn.getState();
+  channel[4] = (String)bs;
+  channel[4] = e + channel[4];
   if(bs == 1) {
-    charLine(p1s, display, 0, 0 + (rowSpan * 0));
-    //charStr(p1s, display, 0, 1 + (rowSpan * 0));
-    charLine(p2s, display, 0, 2 + (rowSpan * 1));
-    //charStr(p2s, display, 0, 3 + (rowSpan * 1));
-    charLine(p3s, display, 0, 4 + (rowSpan * 2));
-    //charStr(p3s, display, 0, 5 + (rowSpan * 2));
-    charLine(p4s, display, 0, 6 + (rowSpan * 3));
-    //charStr(p4s, display, 0, 7 + (rowSpan * 3));
-    display.display();
     display.clear();
+    for(int i = 0; i < 4; i++) {
+      int x;
+      x = (i + 1) + (colSpan * i) + (colSpan / 2);
+      charLine(ps[i], display, x, OLED::H_64 - 1);
+    }
+    display.display();
     cleared = false;
   }
   else {
@@ -105,11 +105,12 @@ void loop() {
       cleared = true;
       delay(100);
     }
-    Serial.println(channelA);
-    Serial.println(channelB);
-    Serial.println(channelC);
-    Serial.println(channelD);
-    Serial.println(channelE);
+    if(Serial) {
+      for(int i = 0; i < 4; i++) {
+        Serial.println(channel[i]);
+      }
+      Serial.println(channel[4]);
+    }
     delay(50);
   }
 }
